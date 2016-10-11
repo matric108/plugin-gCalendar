c063de3dd366023fbe36226ea7da69d6a0a997f3
Loïc <loic@jeedom.com>
2016-10-11 11:46:09 +0200

    cleaning to rework

--- desktop/js/gCalendar.js
+++ desktop/js/gCalendar.js
@@ -15,75 +15,39 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// pour l'ajout de commande //
+
+$("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+/*
+ * Fonction pour l'ajout de commande, appellé automatiquement par plugin.template
+ */
 function addCmdToTable(_cmd) {
     if (!isset(_cmd)) {
         var _cmd = {configuration: {}};
     }
+    if (!isset(_cmd.configuration)) {
+        _cmd.configuration = {};
+    }
     var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-    tr += '<td><span class="cmdAttr" data-l1key="id" ></span></td>';
-    tr += '<td style="font-size:90%;">';
-		tr += '{{Nom}} : <br/><input class="cmdAttr form-control input-sm" style="width:50%; margin-bottom:3px;" data-l1key="name">';
-		tr += '{{URL de l\'agenda}} : <br/><input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="calendarUrl" style="width:98%;">';
-	tr += '</td>';
-	tr += '<td style="font-size:90%;">';
-		tr += '{{Valeur par défaut}} : <br/><input class="cmdAttr form-control input-sm" style="width:95%; margin-bottom:3px;" data-l1key="configuration" data-l2key="defaultValue">';
-		tr += '{{Format donnée}} : <br/><select class="cmdAttr form-control input-sm" style="width:95%; margin-bottom:3px;" data-l1key="configuration" data-l2key="viewStyle">';
-			tr += '<option value=\'current_titleOnly\'> {{event courant}} </option>';
-			tr += '<option value=\'current_withHour\'> {{event courant (avec heures)}} </option>';
-			tr += '<option value=\'1day_next1hour\'> {{event heure à venir}} </option>';
-			tr += '<option value=\'1day_today\'> {{event sur la journée}} </option>';
-		tr += '</select>';
-		tr += '<input type="checkbox" class="cmdAttr bootstrapSwitch" data-l1key="configuration" data-l2key="indicDebFin" data-label-text="{{Indicateurs début/fin}}" checked/> ';
-//		tr += '<input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="useOffset"/> : {{Activer l\'offset en cas de congé/absence}} <br/>';
-	tr += '</td>';
-	tr += '<td style="font-size:90%;">';
-		tr += '<input type="checkbox" class="cmdAttr bootstrapSwitch" data-l1key="isVisible" data-label-text="{{Afficher calendrier}}" checked/> ';
-		tr += '<input type="checkbox" class="cmdAttr bootstrapSwitch" data-l1key="configuration" data-label-text="{{Afficher heure}}" data-l2key="showHour" checked/> ';
-		tr += '<span style="padding-left:10px;" class="spanShowHour24"><input type="checkbox" class="cmdAttr" data-label-text="{{Afficher heure event de 24h}}" data-l1key="configuration" data-l2key="showHour24H" checked/></span> ';
-	tr += '</td>';
-    tr += '<td style="width:100px;">';
-    tr += '<input class="cmdAttr form-control input-sm" data-l1key="type" value="info" style="display : none;">';
-    tr += '<input class="cmdAttr form-control input-sm" data-l1key="subType" value="string" style="display : none;">';
+    tr += '<td>';
+    tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
+    tr += '</td>';
+    tr += '<td>';
+    tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
+    tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
+    tr += '</td>';
+    tr += '<td>';
     if (is_numeric(_cmd.id)) {
         tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i></a> ';
         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
     }
-    tr += '<i class="fa fa-minus-circle pull-right cmdAction" data-action="remove"></i></td>';
+    tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>';
+    tr += '</td>';
     tr += '</tr>';
     $('#table_cmd tbody').append(tr);
     $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
+    if (isset(_cmd.type)) {
+        $('#table_cmd tbody tr:last .cmdAttr[data-l1key=type]').value(init(_cmd.type));
+    }
+    jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
 }
-
-// pour l'aide à l'écriture d'un événement dans l'agenda google //
-$('#bt_helpForWriteGCalEvent').on('click', function (event) {
-	$('#md_modal').dialog({title: "{{Assistance à la création d'un événement \"scénario\" dans un Agenda Google}}"});
-	$('#md_modal').load('index.php?v=d&plugin=gCalendar&modal=help-scenario&id=' + $('.eqLogicAttr[data-l1key=id]').value(), function () {
-		$('#bt_genEventFormat').on('click', function (event) {
-			sEventFormat = "sc=" + $('#gCalendar_idScenario').value();
-			if (($('#gCalendar_variable').value()!='') && (($('#gCalendar_valFirst').value()!='') || ($('#gCalendar_valLast').value()!=''))) {
-				sEventFormat += ";" + $('#gCalendar_texte').value().replace(";", ",");
-				sEventFormat += ";" + $('#gCalendar_variable').value().replace(";", ",");
-				sEventFormat += ";" + $('#gCalendar_valFirst').value().replace(";", ",");
-				sEventFormat += ";" + $('#gCalendar_valLast').value().replace(";", ",");
-			} else if ($('#gCalendar_texte').value()!='') {
-				sEventFormat += ";" + $('#gCalendar_texte').value().replace(";", ",");
-			}
-			$('#gCalendar_EventFormat').value(sEventFormat);
-		});
-		$('.btn-danger[data-dismiss=modal]').on('click', function () {
-			$('#md_modal').dialog('close');
-		});
-	}).dialog('open');
-});
-
-$('body').delegate('.cmd .cmdAttr[data-l1key=configuration][data-l2key=showHour]', 'change', function () {
-	if ($(this).value()==1) {
-		$(this).closest('.cmd').find('.spanShowHour24').show();
-	} else {
-		$(this).closest('.cmd').find('.spanShowHour24').hide();
-	}
-});
-
-
-//$('.eqLogicAttr[data-l2key=showHour]').on('change', function (event) { alert(''); });
--- desktop/php/gCalendar.php
+++ desktop/php/gCalendar.php
@@ -2,8 +2,9 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-sendVarToJS('eqType', 'gCalendar');
-$eqLogics = eqLogic::byType('gCalendar');
+$plugin = plugin::byId('gCalendar');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
@@ -22,44 +23,62 @@ foreach ($eqLogics as $eqLogic) {
    </div>
 
    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend>{{Mes agendas Google}}
-    </legend>
-    <?php
-if (count($eqLogics) == 0) {
-	echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez encore d'agenda Google, cliquez à gauche sur le bouton ajouter un agenda pour commencer}}</span></center>";
-} else {
-	?>
-       <div class="eqLogicThumbnailContainer">
-        <?php
-foreach ($eqLogics as $eqLogic) {
-		echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-		echo "<center>";
-		echo '<img src="plugins/gCalendar/doc/images/gCalendar_icon.png" height="105" width="95" />';
-		echo "</center>";
-		echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-		echo '</div>';
-	}
-	?>
+    <legend>{{Mes Agendas}}</legend>
+  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+  <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+         <center>
+            <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
+    </div>
+      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+    <center>
+      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
   </div>
-  <?php }
+  </div>
+  <legend><i class="fa fa-table"></i> {{Mes Agendas}}</legend>
+<div class="eqLogicThumbnailContainer">
+    <?php
+foreach ($eqLogics as $eqLogic) {
+	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+	echo "<center>";
+	echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+	echo "</center>";
+	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+	echo '</div>';
+}
 ?>
+</div>
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+        </ul>
+
+        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
     <form class="form-horizontal">
         <fieldset>
-            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
             <div class="form-group">
-                <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
-                <div class="col-sm-2">
+                <label class="col-sm-3 control-label">{{Nom de l'équipement agenda}}</label>
+                <div class="col-sm-3">
                     <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement gCalendar"/>
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement agenda}}"/>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-                <div class="col-sm-2">
-                    <select class="eqLogicAttr form-control" data-l1key="object_id">
+                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                <div class="col-sm-3">
+                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
                         <option value="">{{Aucun}}</option>
                         <?php
 foreach (object::all() as $object) {
@@ -69,83 +88,30 @@ foreach (object::all() as $object) {
                    </select>
                </div>
            </div>
-           <div class="form-group">
-            <div class="col-sm-1">
-               <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-               <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
-           </div>
-       </div>
-       <legend></legend>
-       <div class="form-group">
-        <label class="col-sm-2 control-label" >{{Autre Widget}}</label>
-        <div class="col-sm-2" style="width: 4%;">
-            <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-l1key="configuration"  data-l2key="widgetOther"  checked/>
-        </div>
-        <span style="font-size: 80%;">({{A cocher si vous souhaitez utiliser un widget "personnel"; données brutes affichées dans ce cas. Laissez décocher pour utiliser le widget du plugin.}})</span>
-    </div>
     <div class="form-group">
-        <label class="col-sm-2 control-label" >{{Ne pas afficher la date}}</label>
-        <div class="col-sm-2" style="width: 4%;">
-            <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-l1key="configuration"  data-l2key="hideDateDashboard"  checked/>
-        </div>
-        <span style="font-size: 80%;">({{Uniquement pour le Dashboard et Vues}})</span>
-    </div>
-    <legend></legend>
-    <div class="form-group">
-        <label class="col-sm-2 control-label" >{{Autorise les Scénarios}}</label>
-        <div class="col-sm-2" style="width: 4%;">
-            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration"  data-l2key="acceptLaunchSc"  checked/>
-        </div>
-        <span style="font-size: 80%;">({{Permet de lancer un scénario directement depuis le plugin gCalendar. La trame reçue de l'évènement doit être correctement formatée.}})</span><br/>
-        <i class="fa fa-magic cursor" style="color:#0000FF;" id="bt_helpForWriteGCalEvent"></i><span style="font-size:90%;color:#0000FF;"> : {{Aide à la saisie d'un événement dans Google}}</span>
-    </div>
-    <div class="form-group">
-        <label class="col-sm-2 control-label" >{{Fréquence de mise à jour du cache}}</label>
-        <div class="col-sm-2">
-            <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="refreshPeriod">
-                <option value="15">15 min.</option>
-                <option value="30">30 min.</option>
-                <option value="60">1 h.</option>
-                <option value="180">3 h.</option>
-                <option value="360">6 h.</option>
-                <option value="720">12 h.</option>
-                <option value="1440">24 h.</option>
-            </select>
+        <label class="col-sm-3 control-label"></label>
+        <div class="col-sm-9">
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
         </div>
     </div>
 </fieldset>
 </form>
-
-<legend>{{GCalendar}}</legend>
-<div class="alert alert-info" style="font-size:90%;">
- {{L'URL de l'agenda google se trouve dans Paramètres>Agenda>[Agenda voulu]>Adresse privée XML<br/>
- <br/>
- - Pour les éléments de configuration, reportez-vous à la documentation.<br/>
- - Pour une utilisation identique à la V1 du plugin, utiliser le format de donnée "event courant".}}</div>
- <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande google agenda}}</a><br/><br/>
- <table id="table_cmd" class="table table-bordered table-condensed">
+</div>
+      <div role="tabpanel" class="tab-pane" id="commandtab">
+<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+<table id="table_cmd" class="table table-bordered table-condensed">
     <thead>
         <tr>
-            <th style="width: 3%;">{{Id}}</th>
-            <th style="width: 37%;">{{Nom et URL}}</th>
-            <th style="width: 29%;">{{Données d'utilisation}}</th>
-            <th style="width: 23%;">{{Options graphique}}</th>
-            <th style="width: 8%;">{{Action}}</th>
+            <th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
         </tr>
     </thead>
     <tbody>
-
     </tbody>
 </table>
+</div>
+</div>
 
-<form class="form-horizontal">
-    <fieldset>
-        <div class="form-actions">
-            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-        </div>
-    </fieldset>
-</form>
 </div>
 </div>
 
